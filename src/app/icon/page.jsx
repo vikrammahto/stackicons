ce696e4069830abf06a066e8a1e2cec478a752d6
vikrammahto <vikramkr.in@gmail.com>
2025-05-03 00:13:55 +0530

    show only base icon in main page

--- src/app/icon/page.jsx
+++ src/app/icon/page.jsx
@@ -6,12 +6,25 @@ import { icons } from '@/lib/icons';
 const page = () => {
   const [search, setSearch] = useState('');
 
-  const filteredIcons = icons.filter((icon) =>
-    icon.name.toLowerCase().includes(search.toLowerCase()),
-  );
+  const groupIconsByBaseName = (icons) => {
+    const grouped = {};
+    icons.forEach((icon) => {
+      const baseName = icon.name.split('-')[0];
+      if (!grouped[baseName]) {
+        grouped[baseName] = [];
+      }
+      grouped[baseName].push(icon);
+    });
+    return grouped;
+  };
 
+  const groupedIcons = groupIconsByBaseName(icons);
+
+  const filteredIcons = Object.keys(groupedIcons).filter((baseName) =>
+    baseName.toLowerCase().includes(search.toLowerCase()),
+  );
   return (
-    <div className="container py-8">
+    <div className="mx-auto w-full px-4 py-8 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
       <input
         type="text"
         placeholder="Search icons..."
@@ -20,9 +33,11 @@ const page = () => {
         className="mb-6 w-full rounded border px-4 py-2"
       />
 
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
-        {filteredIcons.map((icon, index) => (
-          <IconCard key={index} icon={icon} />
+      <div className="grid grid-cols-3 gap-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8">
+        {filteredIcons.map((baseName, index) => (
+          <div key={index} onClick={() => openModal(baseName)}>
+            <IconCard icon={groupedIcons[baseName][0]} />
+          </div>
         ))}
       </div>
     </div>
